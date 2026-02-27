d6dd888ae934f5828e1890613b3885cc573be68b
Eklavvyaaaaa <eklavya.puri11@gmail.com>
2026-02-27 15:10:03 +0530

    feat: integrate JWT authentication and fix vite api port

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -57,7 +57,7 @@ exports.login = async (req, res) => {
         const token = jwt.sign(
             { id: user.id, role: user.role },
             process.env.JWT_SECRET,
-            { expiresIn: '1d' }
+            { expiresIn: '2h' }
         );
 
         res.json({
